f5dc550e949c54393a0cf9d4be85f196d3418c84
Shekhar Sajwan <shekharsajwan97@gmail.com>
2024-08-29 23:52:30 +0530

    Request log handler added in middleware

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 5000;
+const { logger } = require("./middleware/logger");
+
+app.use(logger);
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
