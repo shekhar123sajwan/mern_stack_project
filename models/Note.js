c525fb55e3acca0f4ce3a19a8a33db793f5860f1
Shekhar Sajwan <shekharsajwan97@gmail.com>
2024-08-30 23:31:08 +0530

    mongo db connection created and implemented on server.js

--- models/Note.js
+++ models/Note.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 
+//Auto Increment ID
+const autoIncrement = require("mogoose-sequence")(mongoose);
+
 const noteSchema = new mongooseSchema(
   {
     user: {
@@ -24,5 +27,11 @@ const noteSchema = new mongooseSchema(
     timestamps: true,
   }
 );
+
+noteSchema.plugin(autoIncrement, {
+  inc_field: "id",
+  id: "ticketNums",
+  start_seq: 500,
+});
 
 module.exports = mongoose.model("Note", noteSchema);
