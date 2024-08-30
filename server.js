711af55e038a33760d5df7abd6087ef25b95f45b
Shekhar Sajwan <shekharsajwan97@gmail.com>
2024-08-30 23:12:28 +0530

    User and Note schema created.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -6,7 +7,6 @@ const { errorHandler } = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
-require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
 app.use(logger);
